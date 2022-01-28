706cc2a4b4020c8783f493b351f1963cba8e1431
boyang <21258210@qq.com>
2022-01-28 09:20:54 -0600

    Add files via upload

--- lookf.py
+++ lookf.py
@@ -15,23 +15,36 @@ import time
 def lookup(p):
 	df = pd.read_excel('c.xlsx')
 	pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-	l = freecarrierlookup.FreeCarrierLookup()
-	image, ocr = l.get_captcha()
-	t = pytesseract.image_to_string(image)
+	
+	for j in range(10):
+		try:
+			l = freecarrierlookup.FreeCarrierLookup()
+			image, ocr = l.get_captcha()
+			t = pytesseract.image_to_string(image)
+			print(t)
+		
+			a = df[df.q == t].iloc[0]['a']
+			try:
+				a = int(a)
+			except:
+				pass
+			print(a)
+			time.sleep(2)
+			
+			i = l.lookup('1', p, a)
+			df = pd.DataFrame(i,index = [0])
+			return df
+		except Exception as e:
+			print(e)
+			print('retrying')
+			time.sleep(2)
+	return pd.DataFrame()
 
-	a = df[df.q == t].iloc[0]['a']
-	try:
-		a = int(a)
-	except:
-		pass
-	time.sleep(5)
-	i = l.lookup('1', p, a)
-	df = pd.DataFrame(i,index = [0])
-	return df
-
-
-p = '4054122710'
+p = '4235135997'
 df = lookup(p)
-print(df)
+if len(df) == 0:
+	print('failed')
+else:
+	print(df)
 
 
