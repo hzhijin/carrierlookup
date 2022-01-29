432593b429658ac9de4c8ebcb7829481b0fe8473
boyang <21258210@qq.com>
2022-01-29 10:55:40 -0600

    Add files via upload

--- p2t.py
+++ p2t.py
@@ -7,11 +7,42 @@ Created on Sat Jan 29 02:42:08 2022
 
 
 import PyPDF2
+import re
+import pandas as pd
 
-pdfFileObj = open('omma_laboratory_list.pdf', 'rb')
+
+def parsePage(rr):
+	namelist = re.findall(r'COUNTY&&&&&(.*?)&&&&&',rr[0])
+	res = []
+	for r in rr[1:]:
+		_lic = 'GAAA' + re.findall(r'&&&&&GAAA(.*?)&&&&&',r)[0]
+		_trader = re.findall(r'e:(.*?)&&&&&',r)[0]
+		_email = re.findall(_lic+r'&&&&&(.*?)&&&&&',r)[0]
+		_phone = re.findall(r'&&&&&(?:\d ?){7,12}&&&&&',r)[0]
+		_zip = re.findall(r'&&&&&(?:\d ?){5,5}&&&&&',r)[0]
+		_county = re.findall(r'(.*?)&&&&&',r.split(_zip)[-1])[0]
+		res.append((_trader,_lic,_email,_phone,_zip,_county,))
+		try:
+			_name2 = re.findall(r'(.*?)&&&&&',r.split(_zip)[-1])[1]
+			namelist.append(_name2)
+		except Exception as e:
+			pass
+	df = pd.DataFrame(res,columns = ['lic','trader','email','phone','zip','county'])
+	df['name'] = namelist
+	return df
+
+
+pdfFileObj = open('omma_growers_list.pdf', 'rb')
+
+
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 print(pdfReader.numPages)
-pageObj = pdfReader.getPage(1)
-t = pageObj.extractText()
-print(t)
+
+for i in range(pdfReader.numPages):
+	pageObj = pdfReader.getPage(i)
+	t = pageObj.extractText().replace('\n \n','&&&&&').replace('\n',' ')
+	rr = t.split('Trade N')
+
 pdfFileObj.close()
+
+df = parsePage(rr)
